994fef0f6c827b0550df813984060a07604413a5
OksanaMeskhi <oks.meskhi@gmail.com>
2023-05-25 22:13:39 +0300

    на ревью

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 def page_not_found(request, exception):
     return render(
         request,
-        'templates/core/404.html',
+        'core/404.html',
         {'path': request.path},
         status=404)
 
 
 def permission_denied(request, exception):
-    return render(request, 'templates/core/403.html', status=403)
+    return render(request, 'core/403.html', status=403)
