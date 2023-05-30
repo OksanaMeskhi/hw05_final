804bdfd20cdfb3c6a566fe673400e8e654f9fd43
OksanaMeskhi <oks.meskhi@gmail.com>
2023-05-30 17:29:13 +0300

    fix

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -133,8 +133,11 @@ def follow_index(request):
 def profile_follow(request, username):
     """Функция подписки на автора."""
     author = get_object_or_404(User, username=username)
-    if author != request.user:
-        Follow.objects.get_or_create(user=request.user, author=author)
+    following = Follow.objects.filter(
+        user=request.user, author=author
+    ).exists()
+    if request.user != author and not following:
+        Follow.objects.create(user=request.user, author=author)
     return redirect('posts:profile', author.username)
 
 
